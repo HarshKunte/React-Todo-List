48a13122da4eb46986365e6815d4c80a5ccf61f3
Harsh Kunte <harshkunte99@gmail.com>
2021-04-16 16:04:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,7 @@ const firebaseApp =  firebase.initializeApp({
     messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
     measurementId:  process.env.REACT_APP_MEASUREMENT_ID
-    // apiKey: "AIzaSyB-yduBzyaEuj2fcaYHJtnJ7NwCbZS91HY",
-    // authDomain: "todo-list-4810f.firebaseapp.com",
-    // projectId: "todo-list-4810f",
-    // storageBucket: "todo-list-4810f.appspot.com",
-    // messagingSenderId: "823754707038",
-    // appId: "1:823754707038:web:c772db0d036c9335ba24cf",
-    // measurementId: "G-D8C87B1L30"
+  
 })
 
 const db = firebaseApp.firestore();
